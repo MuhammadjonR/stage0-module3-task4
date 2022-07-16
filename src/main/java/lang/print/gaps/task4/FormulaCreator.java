d233476cafb70d8f6e0d972bfad607bbecf5f9f3
MuhammadjonR <100756194+MuhammadjonR@users.noreply.github.com>
2022-07-17 00:09:07 +0500

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -4,6 +4,6 @@ public class FormulaCreator {
     public static void main(String[] args) {
     char a = 'a';
      char b = 'b';
-     double s = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+     double s = (9*Math.pow(a,2)-5*b+2+a-7)*((a+b-4*a*b)/2);
     }
 }
